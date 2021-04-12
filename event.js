0eb32c1afaa627fdb1eaa8dc014d3df1f8693f9c
Henrique Costa <henriquecostapsilva@gmail.com>
2021-04-11 23:24:45 -0300

    Utilizando o once

--- event.js
+++ event.js
@@ -1,8 +1,11 @@
 const { EventEmitter } = require('events')
 const ev = new EventEmitter()
 
-ev.on('saySomething', (message) => {
+// o once so vai emitir uma unica vez
+ev.once('saySomething', (message) => {
     console.log('Eu ouvi você:', message)
 })
 
 ev.emit('saySomething', 'Henrique')
+ev.emit('saySomething', 'Camila')
+ev.emit('saySomething', 'Daniel')
